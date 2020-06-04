15ebf797c1edce68e4da2d42cd94a9827a6efc01
leonardomso <leonardomso11@gmail.com>
2020-06-03 21:39:50 -0300

    Commented fucking line

--- packages/web/src/modules/App/Player/Player.tsx
+++ packages/web/src/modules/App/Player/Player.tsx
@@ -13,8 +13,8 @@ const currentPodcast = {
   episode: "EP 928 - Trying to redesign this app",
 };
 
-const file =
-  "https://traffic.omny.fm/d/clips/a858b0a5-e5e6-4a14-9717-a70b010facc1/7d7ad78a-afa7-4af6-b0ab-a7bf00d33acc/af860e31-b511-4f3b-8eb7-a95101178792/audio.mp3?utm_source=Podcast&in_playlist=63b76787-ee2e-44cd-86e1-a8c500bf6f64&t=1536141755";
+// const file =
+//"https://traffic.omny.fm/d/clips/a858b0a5-e5e6-4a14-9717-a70b010facc1/7d7ad78a-afa7-4af6-b0ab-a7bf00d33acc/af860e31-b511-4f3b-8eb7-a95101178792/audio.mp3?utm_source=Podcast&in_playlist=63b76787-ee2e-44cd-86e1-a8c500bf6f64&t=1536141755";
 
 const Player: React.FC = () => {
   return (
