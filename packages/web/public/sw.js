38534f367152c949bd9eba3d465b7a7f8d887edd
leonardomso <leonardomso11@gmail.com>
2020-06-10 17:31:18 -0300

    Removed unused dependencies and installed Rehawk

--- packages/web/public/sw.js
+++ packages/web/public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-6b860c84'], function (workbox) { 'use strict';
     "revision": "5cefc1b419aee4e33b82e423f4f926f8"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "a8a5382e5b119f7bcf64c65b026fd7cb"
+    "revision": "facfd627c7f93f45569c436fb9e77104"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
