b5b07508885743bcab4db82c6c2b5ae4c04d6bf7
leonardomso <leonardomso11@gmail.com>
2020-11-22 09:04:51 -0300

    changes...

--- packages/web/src/components/App/Header/Settings/Settings.tsx
+++ packages/web/src/components/App/Header/Settings/Settings.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { User } from "react-feather";
-import { useQuery, graphql } from "relay-hooks";
+import { fetchQuery } from "relay-runtime";
+import { useQuery, graphql, STORE_OR_NETWORK } from "relay-hooks";
 
 import { SettingsContainer } from "./Settings.styles";
 
 import { useAuthContext } from "src/context/Auth/Auth";
 import { useSettingsContext } from "src/context/Settings/Settings";
 
-import withData from "src/relay/withData";
-
 import Link from "src/system/Link/Link";
 
 import useAuthUser from "src/hooks/useAuthUser";
+
+import { RelayEnvironment } from "src/relay/RelayEnvironment";
 
 import { SettingsQuery } from "./__generated__/SettingsQuery.graphql";
 
@@ -28,8 +29,9 @@ const variables = {};
 const Settings = () => {
   const [, , handleAuth] = useAuthContext();
   const [, handleSettings] = useSettingsContext();
+
   const { props, error } = useQuery<SettingsQuery>(query, variables, {
-    fetchPolicy: "network-only",
+    fetchPolicy: STORE_OR_NETWORK,
   });
 
   if (props) {
@@ -59,10 +61,24 @@ const Settings = () => {
       </SettingsContainer>
     );
   } else if (error) {
+    console.log("error: ", error);
     return <div>error</div>;
   }
 
   return <SettingsContainer>...</SettingsContainer>;
 };
 
-export default withData(Settings, { query });
+Settings.getStaticProps = async () => {
+  const environment = RelayEnvironment();
+  const queryProps = await fetchQuery(environment, query, variables);
+  const initialRecords = environment.getStore().getSource().toJSON();
+
+  return {
+    props: {
+      queryProps,
+      initialRecords,
+    },
+  };
+};
+
+export default Settings;
