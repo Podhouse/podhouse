690d239658f97d003710654233646234353e21b5
leonardomso <leonardomso11@gmail.com>
2021-01-09 09:54:23 -0300

    added Sendgrid

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 const connectDB = () => {
   return new Promise((resolve, reject) => {
-    mongoose.Promise = global.Promise;
     mongoose.connection
       .on("error", (error) => reject(error))
       .on("close", () => console.log("Database connection closed."))
--- packages/server/src/modules/User/mutations/UserSignUpWithEmail.ts
+++ packages/server/src/modules/User/mutations/UserSignUpWithEmail.ts
@@ -1,11 +1,17 @@
 import { GraphQLString, GraphQLNonNull } from "graphql";
 import { mutationWithClientMutationId } from "graphql-relay";
+import dotenv from "dotenv";
+import sgMail from "@sendgrid/mail";
+
+dotenv.config();
 
 import UserModel, { IUser } from "../UserModel";
 
 import { generateToken } from "../../../utils/auth";
 
 import { errorField, successField } from "../../../common/";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 type UserSignUpWithEmailArgs = {
   email: string;
@@ -39,6 +45,25 @@ export default mutationWithClientMutationId({
       email,
       password,
     }).save();
+
+    const userSignUpEmailMessage = {
+      to: email,
+      from: "podhouse@podhouse.app",
+      subject: "Sending with Twilio SendGrid is Fun",
+      text: "and easy to do anywhere, even with Node.js",
+      html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    };
+
+    sgMail.send(userSignUpEmailMessage).then(
+      () => {},
+      (error) => {
+        console.error(error);
+
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      },
+    );
 
     return {
       token: generateToken(user._id),
