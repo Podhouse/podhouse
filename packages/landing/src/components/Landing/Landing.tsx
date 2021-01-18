e7bfb2c41f389e83142242c09025c381ef2d4390
leonardomso <leonardomso11@gmail.com>
2021-01-18 11:02:49 -0300

    Social metadata and fix on Relay

--- packages/landing/src/components/Landing/Landing.tsx
+++ packages/landing/src/components/Landing/Landing.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 import { LandingContainer, LandingInnerContainer } from "./Landing.styles";
 
@@ -9,28 +10,62 @@ import Footer from "src/components/Landing/Footer/Footer";
 
 import theme from "src/system/theme";
 
+import PodhouseImage from "../../../public/icon-1200x630.png";
+
 interface LandingProps {
   children: React.ReactNode;
 }
 
-const Landing = ({ children }: LandingProps) => (
-  <ChakraProvider theme={theme}>
-    <LandingContainer>
-      <LandingInnerContainer>
-        <Head>
-          <title>Podhouse</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Header />
-        {children}
-        <Footer />
-      </LandingInnerContainer>
-    </LandingContainer>
-  </ChakraProvider>
-);
+const Landing = ({ children }: LandingProps) => {
+  const router = useRouter();
+
+  return (
+    <ChakraProvider theme={theme}>
+      <LandingContainer>
+        <LandingInnerContainer>
+          <Head>
+            <title>Podhouse</title>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta charSet="utf-8" />
+            <meta
+              name="description"
+              content="The best podcast web app to listen to your favorite podcasts"
+            />
+            <meta property="og:title" content="Podhouse" key="ogtitle" />+{" "}
+            <meta
+              property="og:description"
+              content="The best podcast web app to listen to your favorite podcasts"
+              key="ogdesc"
+            />
+            {/* Twitter */}
+            <meta name="twitter:card" content="summary" key="twcard" />
+            <meta
+              name="twitter:creator"
+              content="@podhouseapp"
+              key="twhandle"
+            />
+            {/* Open Graph */}
+            <meta property="og:url" content={router.pathname} key="ogurl" />
+            <meta property="og:image" content={PodhouseImage} key="ogimage" />
+            <meta property="og:site_name" content="Podhouse" key="ogsitename" />
+            <meta property="og:title" content="Podhouse" key="ogtitle" />
+            <meta
+              property="og:description"
+              content="The best podcast web app to listen to your favorite podcasts"
+              key="ogdesc"
+            />
+          </Head>
+          <Header />
+          {children}
+          <Footer />
+        </LandingInnerContainer>
+      </LandingContainer>
+    </ChakraProvider>
+  );
+};
 
 export const getLayout = (page) => <Landing>{page}</Landing>;
 
