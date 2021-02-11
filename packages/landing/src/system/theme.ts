13089c03d5f38a71914070230c453e7fa2be627b
leonardomso <leonardomso11@gmail.com>
2021-02-11 10:38:37 -0300

    Fix font on Landing

--- packages/landing/src/system/theme.ts
+++ packages/landing/src/system/theme.ts
@@ -4,7 +4,7 @@ const theme = extendTheme({
   styles: {
     global: {
       "html, body": {
-        fontFamily: "Inter",
+        fontFamily: "Inter, sans-serif",
         fontSize: 16,
       },
     },
