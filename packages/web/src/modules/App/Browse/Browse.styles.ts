fe7319519c7cda7dd169074f04da0ff7db08cba8
leonardomso <leonardomso11@gmail.com>
2021-03-15 21:27:30 -0300

    Content now being rendered correctly, removed the space between items

--- packages/web/src/modules/App/Browse/Browse.styles.ts
+++ packages/web/src/modules/App/Browse/Browse.styles.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const BrowseContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: fit-content;
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: repeat(auto-fill, max-content);
