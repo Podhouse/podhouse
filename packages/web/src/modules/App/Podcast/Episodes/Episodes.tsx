b5559cf43e9755cb8c1cf6aeea141acb53b788ed
leonardomso <leonardomso11@gmail.com>
2021-04-19 17:22:04 -0300

    Added react-virtual for virtualizing the list of Episodes

--- packages/web/src/modules/App/Podcast/Episodes/Episodes.tsx
+++ packages/web/src/modules/App/Podcast/Episodes/Episodes.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useVirtual } from "react-virtual";
 
 import EpisodeItem from "src/components/Podcast/EpisodeItem/EpisodeItem";
 
@@ -12,25 +13,61 @@ type Location = {
   id: number;
 };
 
-const Episodes = () => {
-  const { state } = useLocation<Location>();
+const VirtualizedList = ({ size, children, scrollToIndexRef, ...props }: any) => {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const rowVirtualizer = useVirtual({
+    size,
+    parentRef: parentRef,
+  });
 
-  const { data } = useEpisodes(state.id);
+  if (size === 0) {
+    return null;
+  }
 
   return (
-    <EpisodesContainer>
-      {data.items.map(
-        ({ id, title, description, datePublished, duration }: Episode) => (
-          <EpisodeItem
-            key={id}
-            id={id}
-            title={title}
-            description={description}
-            datePublished={datePublished}
-            duration={duration}
-          />
-        )
-      )}
+    <EpisodesContainer ref={parentRef} {...props}>
+      {rowVirtualizer.virtualItems.map(({ index }) => {
+        return children(index)
+      })}
+    </EpisodesContainer>
+  );
+}
+
+const Episodes = () => {
+  const { state } = useLocation<Location>();
+  const { data } = useEpisodes(state.id);
+  const parentRef = useRef<any>();
+  const rowVirtualizer = useVirtual({
+    size: data.count,
+    parentRef
+  });
+
+  return (
+    <EpisodesContainer ref={parentRef}>
+      <div
+        style={{
+          height: `${rowVirtualizer.totalSize}px`,
+          width: "100%",
+          position: "relative"
+        }}
+      >
+        <VirtualizedList size={data.count}>
+          {(index: any) => {
+            const item = data.items[index];
+
+            return (
+              <EpisodeItem
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                description={item.description}
+                datePublished={item.datePublished}
+                duration={item.duration}
+              />
+            )
+          }}
+        </VirtualizedList>
+      </div>
     </EpisodesContainer>
   );
 };
