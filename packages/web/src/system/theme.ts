b238cab47e569af7577b6e3fa783fc00c08d2205
leonardomso <leonardomso11@gmail.com>
2021-04-24 11:18:20 -0300

    Starting to change default theme from Chakra to Podhouse's own theme.

--- packages/web/src/system/theme.ts
+++ packages/web/src/system/theme.ts
@@ -1,61 +1,88 @@
 import { extendTheme } from "@chakra-ui/react";
+import type { GlobalStyleProps, Styles } from "@chakra-ui/theme-tools";
 import { mode } from "@chakra-ui/theme-tools";
 
-const theme = extendTheme({
-  styles: {
-    global: (props: any) => ({
-      color: mode("#B7B7B7", "whiteAlpha.900")(props),
-      bg: mode("white", "#B7B7B7")(props),
-      lineHeight: "base",
-      "*::placeholder": {
-        color: mode("gray.400", "whiteAlpha.400")(props),
-      },
-      "*, *::before, &::after": {
-        borderColor: mode("gray.200", "whiteAlpha.300")(props),
-        wordWrap: "break-word",
-      },
-      fontFeatureSettings: `"pnum"`,
+const styles: Styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("#FFFFFF", "#151419")(props),
+      fontFeatureSettings: "pnum",
       fontVariantNumeric: "proportional-nums",
       fontSize: 16,
-    }),
-  },
-  components: {
-    Button: {
-      baseStyle: {
-        fontFamily: "Passenger Sans",
-        fontWeight: "normal",
-        fontStyle: "normal",
-        fontSize: "16px",
-        lineHeight: "30px",
-      },
     },
-    Link: {
-      baseStyle: {
-        fontFamily: "Passenger Sans",
-        fontWeight: "normal",
-        fontStyle: "normal",
-        fontSize: "16px",
-        lineHeight: "30px",
-      },
+  }),
+};
+
+const colors = {
+  black: "#101010",
+  gray: {
+    "50": "#FAFAFA",
+    "100": "#F5F5F5",
+    "200": "#EBEBEB",
+    "300": "#E0E0E0",
+    "400": "#D6D6D6",
+    "500": "#CCCCCC",
+    "600": "#C2C2C2",
+    "700": "#B8B8B8",
+    "800": "#B7B7B7",
+    "900": "#6F6F6F",
+  },
+};
+
+const components = {
+  Button: {
+    baseStyle: {
+      color: "gray.900",
+      fontFamily: "Passenger Sans",
+      fontWeight: "normal",
+      fontStyle: "normal",
+      fontSize: "16px",
+      lineHeight: "30px",
     },
-    Text: {
-      baseStyle: {
-        fontFamily: "Passenger Sans",
-        fontWeight: "300",
-        fontStyle: "normal",
-        fontSize: "16px",
-        lineHeight: "30px",
-      },
+  },
+  Link: {
+    baseStyle: {
+      color: "gray.900",
+      fontFamily: "Passenger Sans",
+      fontWeight: "normal",
+      fontStyle: "normal",
+      fontSize: "16px",
+      lineHeight: "30px",
     },
   },
-  fonts: {
-    heading: "Passenger Sans",
-    body: "Passenger Sans",
+  Heading: {
+    baseStyle: {
+      color: "black",
+      fontFamily: "Passenger Sans",
+      fontWeight: "600",
+      fontStyle: "normal",
+      lineHeight: "30px",
+    },
   },
-  config: {
-    initialColorMode: "light",
-    useSystemColorMode: false,
+  Text: {
+    baseStyle: {
+      color: "gray.800",
+      fontFamily: "Passenger Sans",
+      fontWeight: "300",
+      fontStyle: "normal",
+      fontSize: "16px",
+      lineHeight: "30px",
+    },
   },
+};
+
+const fonts = {
+  heading: "Passenger Sans",
+  body: "Passenger Sans",
+};
+
+const theme = extendTheme({
+  initialColorMode: "light",
+  useSystemColorMode: true,
+  styles,
+  colors,
+  components,
+  fonts,
 });
 
 export default theme;
