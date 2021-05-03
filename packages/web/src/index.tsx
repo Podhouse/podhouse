049c77af01d00d19eeddea75cb67627d734ffdcc
leonardomso <leonardomso11@gmail.com>
2021-05-03 10:36:12 -0300

    Added Sentry and fixed env variables problem

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 
 import Provider from "src/components/Provider/Provider";
 
@@ -8,6 +10,16 @@ import reportWebVitals from "./reportWebVitals";
 
 import "@reach/slider/styles.css";
 import "@reach/combobox/styles.css";
+
+Sentry.init({
+  dsn:
+    "https://622597cbeb68466eb8f9b0a03180a1aa@o316575.ingest.sentry.io/1799747",
+  integrations: [new Integrations.BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
--- packages/web/src/queries/useSearchPodcast.ts
+++ packages/web/src/queries/useSearchPodcast.ts
@@ -4,13 +4,13 @@ import { useQuery } from "react-query";
 
 import { PodcastsResult, Podcast } from "./types";
 
-const API_SECRET: string = "x6QP5r$YqsUQdBfKuqUB4rdn8cSbNT9fTccBcjx2";
-
 const apiHeaderTime = Math.floor(Date.now() / 1000);
 const sha1Algorithm: string = "sha1";
 const sha1Hash = crypto.createHash(sha1Algorithm);
 const data4Hash: string =
-  process.env.REACT_APP_API_KEY + API_SECRET + apiHeaderTime;
+  (process.env.REACT_APP_API_KEY as string) +
+  (process.env.REACT_APP_API_SECRET as string) +
+  apiHeaderTime;
 sha1Hash.update(data4Hash);
 const hash4Header: string = sha1Hash.digest("hex");
 
@@ -22,7 +22,7 @@ const useSearchPodcast = (text: string) => {
       "X-Auth-Date": "" + apiHeaderTime,
       "X-Auth-Key": process.env.REACT_APP_API_KEY,
       Authorization: hash4Header,
-      "User-Agent": "SuperPodcastPlayer/1.8",
+      "User-Agent": "Podhouse",
     },
   };
 
